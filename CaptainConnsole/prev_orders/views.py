924c338aee04077aac8bfdf0a4acfbe270460ce9
Matthildur Fríða Gunnarsdóttir <mattfrida@gmail.com>
2020-05-04 19:40:01 +0000

    prev orders set up in html(only text)

--- CaptainConnsole/prev_orders/views.py
+++ CaptainConnsole/prev_orders/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+prevorders = [
+    {'name': 'super mario', 'price': 30},
+    {'name': 'tetris', 'price': 17}
+]
 # Create your views here.
 def index(request):
-    return render(request, 'prev_orders/index.html')
+    return render(request, 'prev_orders/index.html', context={'prevorders': prevorders})

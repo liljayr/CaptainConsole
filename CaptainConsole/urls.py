6f49048b75b9554afbda433eaa7bbbbb6a640268
liljag18 <liljag18@ru.is>
2020-05-04 10:44:44 +0000

    small commet in urls.py about admin localhost

--- CaptainConsole/urls.py
+++ CaptainConsole/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+''' localhost/3000/admin/ '''
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]

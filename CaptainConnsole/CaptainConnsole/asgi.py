2617b193e516773426dc3c869c4bb0cbda44c5f9
liljag18 <liljag18@ru.is>
2020-05-04 11:26:10 +0000

    okay, now it should be fixed, lets keep our fingeers crossed though

--- CaptainConnsole/CaptainConnsole/asgi.py
+++ CaptainConnsole/CaptainConnsole/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Captain_Console project.
+ASGI config for CaptainConnsole project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Captain_Console.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CaptainConnsole.settings')
 
 application = get_asgi_application()
--- CaptainConnsole/manage.py
+++ CaptainConnsole/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Captain_Console.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CaptainConnsole.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

5642934443c13c3f60dcdb7bad64989f2a5b1357
Bergþóra Ólöf <bergthoraolof@gmail.com>
2020-05-05 20:56:38 +0000

    cart & checkout html & css

--- CaptainConnsole/cart/urls.py
+++ CaptainConnsole/cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     # http://localhost:8000/manufacturers
     path('', views.index, name="cart-index"),
+    path('checkout/', views.checkout, name="checkout-index"),
+    path('success/', views.success, name="success-index")
 ]

abad8d7d6cc6f135329e0011bd3444a9494c5a11
liljayr <liljayr@gmail.com>
2020-05-05 22:19:30 +0000

    added so that it goes automatically to the homepage on load (in case that wasn't already there) and started setting up games with flex, still needs a bit of work

--- CaptainConnsole/games/views.py
+++ CaptainConnsole/games/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 games = [
-    {'name': 'Super Mario Bros', 'price': 40.99},
-    {'name': 'Tetris', 'price': 15.99}
+    {'name': 'Super Mario Bros', 'price': 40.99, 'image': 'SI_3DSVC_SuperMarioBros.jpg'},
+    {'name': 'Tetris', 'price': 15.99, 'image': 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Ftetris&psig=AOvVaw3kKNo_X5fOVvXxzAjOMAv2&ust=1588780555759000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNiPp_iKnekCFQAAAAAdAAAAABAD'}
 ]
 
 # Create your views here.
@@ -11,7 +11,3 @@ def index(request):
     return render(request, 'games/index.html', context={
         'games': games
     })
-
-    #return render(request, 'candy/prev_index.html', context={
-    #    'candies': candies
-    #})

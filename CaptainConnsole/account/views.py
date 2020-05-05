283a550818b81c6348d402031f71beeee3df4ac2
Matthildur Fríða Gunnarsdóttir <mattfrida@gmail.com>
2020-05-05 15:43:16 +0000

    adding to account

--- CaptainConnsole/account/views.py
+++ CaptainConnsole/account/views.py
@@ -15,5 +15,7 @@ viewed = [
 def index(request):
     return render(request, 'account/index.html', context={'favorites': favorites, 'viewed': viewed})
 
-def prev_index(request):
-    return render(request, 'account/prev_index.html', context={'favorites': favorites, 'viewed': viewed})
+def index2(request):
+    return render(request, 'account/prev_index.html')
+
+

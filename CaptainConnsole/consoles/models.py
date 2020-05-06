b449391a470b3d7adb24b12deefa1f3b6190f27b
liljayr <liljayr@gmail.com>
2020-05-06 15:00:51 +0000

    fixed database issues

--- CaptainConnsole/consoles/models.py
+++ CaptainConnsole/consoles/models.py
@@ -6,10 +6,10 @@ class ConsoleCategory(models.Model):
 class Consoles(models.Model):
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=999)
-    category = models.ForeignKey(ConsoleCategory, on_delete=models.CASCADE())
+    category = models.ForeignKey(ConsoleCategory, on_delete=models.CASCADE)
     price = models.FloatField()
     onSale = models.BooleanField()
 
 class GameImage(models.Model):
     image = models.CharField(max_length=999)
-    game = models.ForeignKey(Consoles, on_delete=models.CASCADE())
+    game = models.ForeignKey(Consoles, on_delete=models.CASCADE)

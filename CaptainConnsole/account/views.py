832dcf73f75dc9bc48356157ccd27e805035e31a
Matthildur Fríða Gunnarsdóttir <mattfrida@gmail.com>
2020-05-06 01:08:01 +0000

    css for account and prev order

--- CaptainConnsole/account/views.py
+++ CaptainConnsole/account/views.py
@@ -11,11 +11,16 @@ viewed = [
     {'name': 'tetris'}
 ]
 
+prev_orders = [
+    {'name': 'super mario', 'price': 30},
+    {'name': 'tetris', 'price': 20}
+]
+
 # Create your views here.
 def index(request):
     return render(request, 'account/index.html', context={'favorites': favorites, 'viewed': viewed})
 
 def index2(request):
-    return render(request, 'account/prev_orders.html')
+    return render(request, 'account/prev_orders.html', context={'prev_orders': prev_orders})
 
 

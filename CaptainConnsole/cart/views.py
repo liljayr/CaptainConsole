d712b69ac0ae740471ecb934ae05854e632efcf4
Bergþóra Ólöf <bergthoraolof@gmail.com>
2020-05-06 19:00:53 +0000

    checkout & confirmation

--- CaptainConnsole/cart/views.py
+++ CaptainConnsole/cart/views.py
@@ -8,6 +8,7 @@ cart = [
     {'name': 'Tetris', 'amount': 1, 'price': 40.00}
 ]
 total_price = 0
+num_in_cart = len(cart)
 for item in range(len(cart)):
     total_price += cart[item].get('price')
 
@@ -16,7 +17,12 @@ def index(request):
     return render(request, 'cart/index.html', context={ 'cart' : cart, 'total_price' : total_price })
 
 def checkout(request):
-    return render(request, 'cart/checkout.html')
+    return render(request, 'cart/checkout.html', context={  'cart' : cart, 'total_price' : total_price ,'num_in_cart' : num_in_cart})
+
+def confirmation(request):
+    return render(request, 'cart/confirmation.html', context={  'cart' : cart, 'total_price' : total_price, 'num_in_cart' : num_in_cart})
 
 def success(request):
     return render(request, 'cart/success.html')
+
+

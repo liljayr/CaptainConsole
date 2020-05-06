bf3d3d60e630309bb6cf4d0718a475f65521af47
liljayr <liljayr@gmail.com>
2020-05-06 10:43:58 +0000

    started on the db tables for games and consoles

--- CaptainConnsole/consoles/models.py
+++ CaptainConnsole/consoles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class ConsoleCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+class Consoles(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=999)
+    category = models.ForeignKey(ConsoleCategory, on_delete=models.CASCADE())
+    price = models.FloatField()
+    onSale = models.BooleanField()
+
+class GameImage(models.Model):
+    image = models.CharField(max_length=999)
+    game = models.ForeignKey(Consoles, on_delete=models.CASCADE())
--- CaptainConnsole/games/models.py
+++ CaptainConnsole/games/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+#id column is default created as primary key, autoincrement
+class GameCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+class Games(models.Model):
+    name = models.CharField(max_length=255)     #blank = True makes it a non required field
+    description = models.CharField(max_length=999)
+    category = models.ForeignKey(GameCategory, on_delete=models.CASCADE())
+    price = models.FloatField()
+    onSale = models.BooleanField()
+    console = models.ForeignKey()       #TODO: Map to console
+
+class GameImage(models.Model):
+    image = models.CharField(max_length=999)
+    game = models.ForeignKey(Games, on_delete=models.CASCADE())

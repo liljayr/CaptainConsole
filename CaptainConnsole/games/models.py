9b5c2b25c8844b73f14c07bb1b849abc6507e85a
liljayr <liljayr@gmail.com>
2020-05-06 10:47:57 +0000

    changed gitignore a bit

--- CaptainConnsole/games/models.py
+++ CaptainConnsole/games/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from CaptainConnsole.consoles.models import Consoles
 
 #id column is default created as primary key, autoincrement
+
 class GameCategory(models.Model):
     name = models.CharField(max_length=255)
 
@@ -10,7 +12,7 @@ class Games(models.Model):
     category = models.ForeignKey(GameCategory, on_delete=models.CASCADE())
     price = models.FloatField()
     onSale = models.BooleanField()
-    console = models.ForeignKey()       #TODO: Map to console
+    console = models.ForeignKey(Consoles, on_delete=models.CASCADE())       #TODO: Map to console
 
 class GameImage(models.Model):
     image = models.CharField(max_length=999)

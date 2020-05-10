d8a24fa90608246730cd1a4e6e79254bd1e2624a
Matthildur Fríða Gunnarsdóttir <mattfrida@gmail.com>
2020-05-10 04:10:50 +0000

    pictures added to favorites

--- CaptainConnsole/account/views.py
+++ CaptainConnsole/account/views.py
@@ -12,17 +12,17 @@ def index(request):
     return renderTemplate(request, 'account/login.html')
 
 def get_account_id(request, id):
-    return renderTemplate(request, 'account/index.html', {'account': get_object_or_404(Accounts, pk=id),
-                                                          'favorites': Favorites.objects.filter(account_id=id)})
+    context = {'account': get_object_or_404(Accounts, pk=id), 'favorites': Favorites.objects.filter(account_id=id),
+               'game': Games.objects.all()}
+    return renderTemplate(request, 'account/index.html', context)
 
 def prev_orders(request, id):
-    return renderTemplate(request, 'account/prev_orders.html', {'account': get_object_or_404(Accounts, pk=id),
-                                                                'orders': Orders.objects.filter(account_id=id, ordered=True)
-                                                                })
+    context = {'account': get_object_or_404(Accounts, pk=id), 'orders': Orders.objects.filter(account_id=id, ordered=True)}
+    return renderTemplate(request, 'account/prev_orders.html', context)
 
 def edit(request, id):
-    return renderTemplate(request, 'account/edit.html', {'account': get_object_or_404(Accounts, pk=id),
-                                                         'favorites': Favorites.objects.filter(account_id=id)})
+    context = {'account': get_object_or_404(Accounts, pk=id), 'favorites': Favorites.objects.filter(account_id=id)}
+    return renderTemplate(request, 'account/edit.html', context)
 
 def login(request):
     return renderTemplate(request, 'account/login.html')

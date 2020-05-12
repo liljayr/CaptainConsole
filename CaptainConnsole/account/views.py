a61049180708fac8eb86c4c56fb95b2fb7d15590
Matthildur Fríða Gunnarsdóttir <mattfrida@gmail.com>
2020-05-12 10:39:58 +0000

    fixes

--- CaptainConnsole/account/views.py
+++ CaptainConnsole/account/views.py
@@ -3,15 +3,7 @@ from django.shortcuts import get_object_or_404
 from account.models import *
 from common.renderTemplates import renderTemplate
 
-
-
-
-
-# Create your views here.
-def index(request):
-    return renderTemplate(request, 'account/login.html')
-
-def get_account_id(request, id):
+def find_fav(id):
     fav = Favorites.objects.filter(account_id=id)
     fav_games = []
     fav_consoles = []
@@ -22,10 +14,16 @@ def get_account_id(request, id):
         except:
             if prod.console_id:
                 fav_consoles.append(prod)
+    return fav_games, fav_consoles
 
-    #context = {'account': get_object_or_404(Accounts, pk=id), 'favorites': Favorites.objects.filter(account_id=id),
-               #'game': Games.objects.all(), }
-    context = {'account': get_object_or_404(Accounts, pk=id), 'game': Games.objects.all(), 'fav_games': fav_games, 'fav_consoles': fav_consoles}
+# Create your views here.
+def index(request):
+    return renderTemplate(request, 'account/login.html')
+
+def get_account_id(request, id):
+    fav_games, fav_consoles = find_fav(id)
+    context = {'account': get_object_or_404(Accounts, pk=id), 'game': Games.objects.all(),
+               'fav_games': fav_games, 'fav_consoles': fav_consoles}
     return renderTemplate(request, 'account/index.html', context)
 
 def prev_orders(request, id):
@@ -33,7 +31,10 @@ def prev_orders(request, id):
     return renderTemplate(request, 'account/prev_orders.html', context)
 
 def edit(request, id):
-    context = {'account': get_object_or_404(Accounts, pk=id), 'favorites': Favorites.objects.filter(account_id=id)}
+
+    fav_games, fav_consoles = find_fav(id)
+    context = {'account': get_object_or_404(Accounts, pk=id), 'favorites': Favorites.objects.filter(account_id=id),
+               'fav_games': fav_games, 'fav_consoles': fav_consoles}
     return renderTemplate(request, 'account/edit.html', context)
 
 def login(request):

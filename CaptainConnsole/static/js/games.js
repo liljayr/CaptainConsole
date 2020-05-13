bc9cc6614c6c706ad342a65d036a42044e54eab0
liljayr <liljayr@gmail.com>
2020-05-13 00:23:57 +0000

    checkboxes almost work, super close to getting filter up and running

--- CaptainConnsole/static/js/games.js
+++ CaptainConnsole/static/js/games.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('#search-btn').on( 'click', function (e) {
         e.preventDefault();
         var searchText = $('#search-box').val();
-        console.log("blahblah");
         $.ajax({
             url: '/games?search_filter=' + searchText,
             type: 'GET',
@@ -10,9 +9,9 @@ $(document).ready(function(){
                 var newHtml = resp.data.map(d => {
                     return `<div class="well-game">
                                 <a href="/games/${d.id}">
-                                    <img class="game-img" src="${d.firstImage}"/>
+                                    <img class="game-img" src="${d.first_image}"/>
                                     <h4>${d.name}</h4>
-                                    <p>${d.description}</p>
+                                    <h4>$${d.price}</h4>
                                 </a>
                             </div> `
                 });
@@ -25,4 +24,55 @@ $(document).ready(function(){
             }
         });
     });
+});*/
+
+$(document).ready(function(){
+    $('#search-btn').on('click', function(e) {
+        e.preventDefault();
+        performSearch();
+    });
+    console.log("console check");
+    console.log($('#consoles'));
+    $('#consoles').on('click', function(e) {
+        performSearch();
+    })
 });
+
+//values in quotes are id in template
+function performSearch(){
+    var query = $('#search-box').val();
+    var sort = $('#sort-by').val();
+    var consoles = new Array();
+    var count = 0;
+    $('#consoles :checked').each(function(index) {
+        //consoles = consoles
+        console.log($(this));
+        console.log(this.value);
+        consoles[count] = this.value;
+        count = count + 1;
+    });
+    console.log(consoles);
+    var types = '';
+    var comp_url = '/games?search_filter=' + query + '&check=' + consoles; //+ '&sort_by=' + sort
+    $.ajax({
+        url: comp_url,
+        type: 'GET',
+        success: function(resp){
+            var newHtml = resp.data.map(d => {
+                return `<div class="well-game">
+                            <a href="/games/${d.id}">
+                                <img class="game-img" src="${d.first_image}"/>
+                                <h4>${d.name}</h4>
+                                <h4>$${d.price}</h4>
+                            </a>
+                        </div> `
+            });
+            $('.games').html(newHtml.join(''));
+            $('#search-box').val('');
+        },
+        error: function (xhr, status, error) {
+            // TODO: Show toastr
+            console.error(error);
+        }
+    });
+};

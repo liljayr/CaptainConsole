658420b7b0de0c7ad7290ee1dce2e14f5998f603
liljayr <liljayr@gmail.com>
2020-05-14 01:27:33 +0000

    on sale application has been started

--- CaptainConnsole/sales/views.py
+++ CaptainConnsole/sales/views.py
@@ -6,6 +6,9 @@ from games.models import Games
 
 def index(request):
     consoles = Consoles.objects.filter(onSale=True)
+    print("CHECK IT OUT!!!!")
+    print(consoles)
     games = Games.objects.filter(onSale=True)
+    games = games.exclude(description=' ')
     context = {'consoles': consoles, 'games': games}
     return renderTemplate(request, 'sales/index.html', context)
